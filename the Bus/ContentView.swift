b3754518270be0cc7a7dd67b86f586514d9ddc24
chshroy <bikingyes@gmail.com>
2020-01-31 22:55:27 +0800

    Initial Commit

---
+++ the Bus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  the Bus
+//
+//  Created by royyaha on 31.01.20.
+//  Copyright © 2020 royyaha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
